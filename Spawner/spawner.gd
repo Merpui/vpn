6a3bf65f36d2cb7bff286892dd358f0f55aab31b
Miguel Escobar Rojas <miguel.escobar-rojas@web.de>
2025-01-25 16:13:57 +0100

    Adjust Spawner to spawn bubbles in a row

--- Spawner/spawner.gd
+++ Spawner/spawner.gd
@@ -1,18 +1,22 @@
 extends Node2D
 
-@export var bubble_scene: PackedScene
-@export var spawn_count: int = 15
+@export var bubble_scene: PackedScene  # Reference to the Bubble scene
+@export var spawn_range: Vector2 = Vector2(200, 300)  # Range for spawn position (x and y)
+@export var num_bubbles: int = 10  # Number of bubbles to spawn
+@export var bubble_spacing: float = 0.1  # Space between bubbles (scaled size)
 
 func _ready():
-	randomize()  # Randomize RNG for varied results
-	for i in range(spawn_count):
-		spawn_bubble()
+	spawn_bubbles_in_row()
 
-func spawn_bubble():
-	var bubble = bubble_scene.instantiate().duplicate()
-	bubble.position = Vector2(randi() % 1200, randi() % 600)
-	
-	bubble.object_type = randi() % 3 + 1
-	bubble.velocity = Vector2(randf() * 500 - 100, randf() * 500 - 100)
-	
-	add_child(bubble)
+func spawn_bubbles_in_row():
+	var bubble_instance = bubble_scene.instantiate()
+	var bubble_sprite = bubble_instance.get_node("Bubble")
+	var bubble_width = bubble_sprite.texture.get_width() * bubble_instance.size
+
+	for i in range(num_bubbles):
+		var spawn_x = spawn_range.x + i * bubble_width
+
+		var current_bubble = bubble_scene.instantiate()
+		current_bubble.position = Vector2(spawn_x, spawn_range.y)
+		current_bubble.object_type = randi_range(1, 3)
+		add_child(current_bubble)
